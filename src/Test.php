1a57fc6255e1bf16613237a5453407add7ff9dc2
tangfeipeng <2623464012@qq.com>
2020-03-05 05:08:03 +0800

    aa

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Tests;
+
+class Tests
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2016!<br />";
+    }
+}
